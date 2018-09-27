0e5e823a612cc556e9ace7a5e41b452f246372b3
maltsev <maltsev@kribrum.ru>
2018-09-27 16:32:00 +0300

    logging added

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-p', dest='show_pairs', action='store_true', default=False, help='available pairs will be shown on each step')
 args = parser.parse_args()
 
-if __name__ == '__main__':
-    
+def main():
+
+    log = open('log', 'w', encoding='utf8').close()
+
     g = Grid()
     g.show_pairs = args.show_pairs
     g.addNums(g.add_nums_position, g.strt_nums)
@@ -18,26 +20,40 @@ if __name__ == '__main__':
 
     while True:
 
-        while len(g.pairlist) > 0:
+        if list(set(g.grid)) != [g.sep]:
 
-            if g.show_pairs:
-                g.printPairlist()
+            while len(g.pairlist) > 0:
 
-            users_pair = g.getPairCoords()
-            if users_pair in g.pairlist:
-                for coord in users_pair:
-                    g.substByCoordinate(coord)
-                g.refreshGrid()
-                g.renderGrid()
-            else:
-                g.refreshGrid()
-                g.renderGrid(message = 'no such pair!')
+                if g.show_pairs:
+                    g.printPairlist()
 
-        new_nums_row = g.getNewNumsRow()
-        g.addNums(g.add_nums_position, new_nums_row)
-        g.refreshGrid()
-        g.renderGrid(message = 'no pairs left, new row added')
+                users_pair = g.getPairCoords()
+                if users_pair[0] in g.pairlist:
+                    log = open('log', 'a', encoding='utf8')
+                    print('%s\t%s'%(users_pair[0], users_pair[1]), file=log)
+                    log.close()
+                    for coord in users_pair[0]:
+                        g.substByCoordinate(coord)
+                    g.refreshGrid()
+                    g.renderGrid()
+                else:
+                    g.refreshGrid()
+                    g.renderGrid(message = 'no such pair!')
+
+            new_nums_row = g.getNewNumsRow()
+            g.addNums(g.add_nums_position, new_nums_row)
+            log = open('log', 'a', encoding='utf8')
+            print('new nums row added', file=log)
+            log.close()
+            g.refreshGrid()
+            g.renderGrid(message = 'no pairs left, new row added')
+
+        else:
+
+            print('congrats, you won!')
+            print('stats are going to be printed out here')
+            break
 
 
-
-
+if __name__ == '__main__':
+    main()
